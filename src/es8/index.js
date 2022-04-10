ab3ae8edb7d35e6973061d87a9ea81d77a6bd50f
esemiliogarra <elmaloy@gmail.com>
2022-04-09 20:28:19 -0500

    upload es8

---
+++ src/es8/index.js
@@ -0,0 +1,13 @@
+const data = {
+    frontend: 'Axel',
+    backend: 'Newton',
+    design: 'loco',
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+const string = 'hello';
+console.log(string.padStart(7,'hi'));
+console.log(string.padEnd(12, '-----'));
