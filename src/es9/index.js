338e06b90d089837bac0dec08330c7543494084b
esemiliogarra <elmaloy@gmail.com>
2022-04-09 20:56:00 -0500

    update

--- src/es9/index.js
+++ src/es9/index.js
@@ -20,3 +20,11 @@ helloWorld()
   .catch(error => console.log(error))
   .finally(() => console.log('Finally'))
 
+
+  const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+  const match = regexData.exec('2018-04-20');
+  const year = match [1]
+  const month = match[2]
+  const day = match[3]
+
+  console.log(year,month, day);
