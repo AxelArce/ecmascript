28ba035778524da5a46e55894e38ee42f3e236ca
esemiliogarra <elmaloy@gmail.com>
2022-04-09 21:55:57 -0500

    upadte

--- src/es11/index.js
+++ src/es11/index.js
@@ -11,12 +11,6 @@ const anotherBigNumber = BigInt(90071992540991);
 console.log(aBigNumber);
 console.log(anotherBigNumber);
 
-const promise1 = new Promise((resolve, reject) => reject("reject"));
-const promise3 = new Promise((resolve, reject) => resolve("resolve"));
-const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
 
-Promise.allSettled([promise1, promise2, promise3])
-    .then(response => console.log(response));
-
-    const fooo = null ?? 'default string';
-    console.log(fooo);
+const fooo = null ?? 'default string';
+console.log(fooo);
