88c7efada3ae949458cbf14cf52ed203a080e1f2
esemiliogarra <elmaloy@gmail.com>
2022-04-09 21:16:06 -0500

    es10

---
+++ src/es10/index.js
@@ -0,0 +1,12 @@
+let array = [1,2,3, [1,2,3, [1,2,3]]];
+
+console.log(array.flat(2))
+
+let hello = '     hello world';
+console.log(hello);
+console.log(hello.trimStart());
+
+let hello2 = 'hello World       ';
+console.log(hello2);
+console.log(hello2.trimEnd());
+
