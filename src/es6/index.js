2e330fee49df925f0c703fa9b9842d5c4a1f04e6
esemiliogarra <elmaloy@gmail.com>
2022-04-09 20:00:45 -0500

    coomit2

--- src/es6/index.js
+++ src/es6/index.js
@@ -93,3 +93,15 @@ helloPromise()
   import { hello } from './module.js';
 
   hello();
+
+  function* helloWorld() {
+      if (true) {
+          yield 'Hello';
+      }
+      if (true) {
+          yield 'World';
+      }
+  };
+
+  const generatorHello = helloWorld();
+  console.log(generatorHello.next(value));
