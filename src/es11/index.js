a03bc4cce05280634d0096cfcdccbc44fae27f6d
esemiliogarra <elmaloy@gmail.com>
2022-04-09 21:49:13 -0500

    update

--- src/es11/index.js
+++ src/es11/index.js
@@ -4,3 +4,16 @@ button.addEventListener("click", async function () {
     const module = await import("./file.js");
     module.hello
 });
+
+const aBigNumber = 90071992540991n;
+const anotherBigNumber = BigInt(90071992540991);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
