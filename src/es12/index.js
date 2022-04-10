890ca3f8dbb70cd3ed91b68a59363b14a2525046
esemiliogarra <elmaloy@gmail.com>
2022-04-09 22:08:26 -0500

    update

--- src/es12/index.js
+++ src/es12/index.js
@@ -5,3 +5,10 @@ console.log(replacedString);
 
 const replacedString2 = string.replaceAll("JavaScript", "Python");
 console.log(replacedString2);
+
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promisee = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+    .then(response => console.log(response));
